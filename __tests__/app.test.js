d25f0a4b945a0c3f7fe0cb8f51e4a1c426440d84
Kalan Prudhomme <cleaver9@gmail.com>
2021-06-17 17:32:09 -0700

    added find all route, test pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,4 +25,22 @@ describe('layered routes', () => {
         expect(res.body).toEqual(order);
       });
   });
+
+  it('gets all orders', async () => {
+    const order1 = await Order.insert({
+      quantity: 8008
+    });
+
+    const order2 = await Order.insert({
+      quantiy: 69
+    });
+
+    const order3 = await Order.insert({
+      quantity: 420
+    });
+  
+    const res = await request(app).get('/api/v1/orders');
+    expect(res.body).toEqual([order1, order2, order3]);
+  });
+  
 });
